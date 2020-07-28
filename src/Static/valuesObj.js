324dc4360698422b5aa6b71f0f0b474148bcb132
JChase2 <mr.jdchase@gmail.com>
2020-07-28 17:06:52 -0400

    Got a new object file where I'll store results for the basic app. It's basically just nested objects starting with size, going down to grade, then threading. This should be fairly easy to adjust later on when/if needed. I'm sure I'll be adding much more data to this file. ResultComp (which I originally thought about doing case or if/else for), now can simply search the object file for matching values down the object tree.

---
+++ src/Static/valuesObj.js
@@ -0,0 +1,64 @@
+const valuesObj = {
+    '1/4': {
+        'grade 2': {
+            threading: {
+                course: 4,
+                fine: 4.7
+            }
+        },
+        'grade 5': {
+            threading: {
+                course: 6.3,
+                fine: 7.3
+            }
+        }, 
+        'grade 8': {
+            threading: {
+                course: 9,
+                fine: 10
+            }
+        }, 
+    },
+    '5/16': {
+        'grade 2': {
+            threading: {
+                course: 8,
+                fine: 9
+            }
+        },
+        'grade 5': {
+            threading: {
+                course: 13,
+                fine: 14
+            }
+        }, 
+        'grade 8': {
+            threading: {
+                course: 18,
+                fine: 20
+            }
+        }, 
+    },
+    '3/8': {
+        'grade 2': {
+            threading: {
+                course: 15,
+                fine: 17
+            }
+        },
+        'grade 5': {
+            threading: {
+                course: 23,
+                fine: 26
+            }
+        }, 
+        'grade 8': {
+            threading: {
+                course: 33,
+                fine: 37
+            }
+        }, 
+    }
+}
+
+export default valuesObj;
