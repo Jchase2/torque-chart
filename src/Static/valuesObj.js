2edf1bd5bd398f5d0ad633cc142a64c076ca4b34
JChase2 <mr.jdchase@gmail.com>
2020-07-29 10:15:35 -0400

    Filling out some values from a chart

--- src/Static/valuesObj.js
+++ src/Static/valuesObj.js
@@ -15,6 +15,12 @@ const valuesObj = {
                                 'UNC': 6.3,
                                 'UNF': 7.3
                             }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 9,
+                                'UNF': 10
+                            }
                         }
                     }
                 },
@@ -24,6 +30,172 @@ const valuesObj = {
                             'threading': {
                                 'UNC': 8,
                                 'UNF': 9
+                            }
+                        },
+                        'Grade 5': {
+                            'threading': {
+                                'UNC': 13,
+                                'UNF': 14
+                            }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 18,
+                                'UNF': 20
+                            }
+                        }
+                    }
+                },
+                '3/8': {
+                    'grade': {
+                        'Grade 2': {
+                            'threading': {
+                                'UNC': 15,
+                                'UNF': 17
+                            }
+                        },
+                        'Grade 5': {
+                            'threading': {
+                                'UNC': 23,
+                                'UNF': 26
+                            }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 33,
+                                'UNF': 37
+                            }
+                        }
+                    }
+                },
+                '7/16': {
+                    'grade': {
+                        'Grade 2': {
+                            'threading': {
+                                'UNC': 24,
+                                'UNF': 27
+                            }
+                        },
+                        'Grade 5': {
+                            'threading': {
+                                'UNC': 37,
+                                'UNF': 41
+                            }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 52,
+                                'UNF': 58
+                            }
+                        }
+                    }
+                },
+                '1/2': {
+                    'grade': {
+                        'Grade 2': {
+                            'threading': {
+                                'UNC': 37,
+                                'UNF': 41
+                            }
+                        },
+                        'Grade 5': {
+                            'threading': {
+                                'UNC': 57,
+                                'UNF': 64
+                            }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 80,
+                                'UNF': 90
+                            }
+                        }
+                    }
+                },
+                '9/16': {
+                    'grade': {
+                        'Grade 2': {
+                            'threading': {
+                                'UNC': 53,
+                                'UNF': 59
+                            }
+                        },
+                        'Grade 5': {
+                            'threading': {
+                                'UNC': 82,
+                                'UNF': 91
+                            }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 115,
+                                'UNF': 129
+                            }
+                        }
+                    }
+                },
+                '5/8': {
+                    'grade': {
+                        'Grade 2': {
+                            'threading': {
+                                'UNC': 73,
+                                'UNF': 83
+                            }
+                        },
+                        'Grade 5': {
+                            'threading': {
+                                'UNC': 112,
+                                'UNF': 128
+                            }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 159,
+                                'UNF': 180
+                            }
+                        }
+                    }
+                },
+                '3/4': {
+                    'grade': {
+                        'Grade 2': {
+                            'threading': {
+                                'UNC': 125,
+                                'UNF': 138
+                            }
+                        },
+                        'Grade 5': {
+                            'threading': {
+                                'UNC': 200,
+                                'UNF': 223
+                            }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 282,
+                                'UNF': 315
+                            }
+                        }
+                    }
+                },
+                '7/8': {
+                    'grade': {
+                        'Grade 2': {
+                            'threading': {
+                                'UNC': 129,
+                                'UNF': 144
+                            }
+                        },
+                        'Grade 5': {
+                            'threading': {
+                                'UNC': 322,
+                                'UNF': 355
+                            }
+                        },
+                        'Grade 8': {
+                            'threading': {
+                                'UNC': 454,
+                                'UNF': 501
                             }
                         }
                     }
