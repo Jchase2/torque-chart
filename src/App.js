60982d970a086961e2590cd42d969946df5da594
JChase2 <mr.jdchase@gmail.com>
2020-08-31 18:54:40 -0400

    Fixed up the App.js component to work by going to /advanced in the url, along with making sure the button stays consistent, also added "Basic" and "Advanced" in the h1 attribute to make suer the user is aware of which version they're using.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import { Button, Grid } from 'semantic-ui-react';
 import UserInput from './Components/UserInput';
 import UserInputAdvanced from './Components/UserInputAdvanced';
-import { Link, Route } from "react-router-dom";
+import { Link, Route, useLocation } from "react-router-dom";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.state = { advanced: false };
+function App(props) {
+
+  let location = useLocation();
+  const returnInitialURL = () => {
+    if(location.pathname === "/"){return false} else return true;
   }
 
-  handleClick() {
-    this.setState({ advanced: !this.state.advanced });
+  const [advanced, setAdvanced] = useState(returnInitialURL);
+
+  let handleClick = () => {
+    setAdvanced(!advanced);
   }
-  render() {
-    return (
-      <Grid container>
-        <div className="center">
-          <h1> Torque Calculator </h1>
-          <Route exact path="/" component={UserInput}/>
-          <Route path="/advanced" component={UserInputAdvanced}/>
-          <p></p>
-          {this.state.advanced ? <Link to="/"><Button onClick={this.handleClick} color='teal'>{this.state.advanced ? 'Basic' : 'Advanced'}</Button>
-          </Link> : <Link to="advanced"><Button onClick={this.handleClick} color='teal'>{this.state.advanced ? 'Basic' : 'Advanced'}</Button>
-            </Link>}
-        </div>
-      </Grid>
-    )
-  }
+  return (
+    <Grid container>
+      <div className="center">
+        {location.pathname === "/" ? <h1>Torque Calculator Basic </h1> : <h1> Torque Calculator Advanced </h1>}
+        <Route exact path="/" component={UserInput} />
+        <Route path="/advanced" component={UserInputAdvanced} />
+        <p></p>
+        {advanced ? <Link to="/"><Button onClick={handleClick} color='teal'>{location.pathname === "/advanced" ? 'Basic' : 'Advanced'}</Button>
+        </Link> : <Link to="advanced"><Button onClick={handleClick} color='teal'>{location.pathname === "/" ? 'Advanced' : 'Basic'}</Button>
+          </Link>}
+      </div>
+    </Grid>
+  )
 }
 
 export default App;
