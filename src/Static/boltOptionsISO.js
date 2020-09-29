28ce7fbc45dbfa6759609333f07d550129154446
JChase2 <mr.jdchase@gmail.com>
2020-09-29 14:11:27 -0400

    Added additional ISO grades.

--- src/Static/boltOptionsISO.js
+++ src/Static/boltOptionsISO.js
@@ -1,6 +1,13 @@
 const boltOptionsISO = [
-    { key: 0, text: 'Grade 8.8', value: 'grade8.8' },
-    { key: 1, text: 'Grade 10.9', value: 'grade10.9' }
+    { key: 0, text: 'Grade 3.6', value: 'grade3.6'},
+    { key: 1, text: 'Grade 4.6', value: 'grade4.6'}, 
+    { key: 2, text: 'Grade 4.8', value: 'grade4.8'}, 
+    { key: 3, text: 'Grade 5.6', value: 'grade5.6'}, 
+    { key: 4, text: 'Grade 5.8', value: 'grade5.8'}, 
+    { key: 5, text: 'Grade 6.8', value: 'grade6.8'}, 
+    { key: 6, text: 'Grade 8.8', value: 'grade8.8' },
+    { key: 7, text: 'Grade 10.9', value: 'grade10.9' }, 
+    { key: 8, text: 'Grade 12.9', value: 'grade12.9'}
 ]
 
 export default boltOptionsISO;
